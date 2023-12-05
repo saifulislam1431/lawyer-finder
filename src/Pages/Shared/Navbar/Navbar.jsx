93849822a4956bddfdbc0dea492aa3ea6d3bef1d
Saiful Islam <sakibbhaiya143@gmail.com>
2023-12-05 12:00:17 +0600

    Navbar responsive added

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { HiChevronRight } from "react-icons/hi2";
 import logo from "../../../assets/logo/logo1.png"
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [isSubOpen, setIsSubOpen] = useState(false);
+
     const navItem = <>
         <li><NavLink to="/" className={({ isActive }) => (isActive ? "navActive" : "navDefault")}>Home</NavLink></li>
 
@@ -14,6 +17,10 @@ const Navbar = () => {
         <li><NavLink to="/blog" className={({ isActive }) => (isActive ? "navActive" : "navDefault")}>Blog <HiChevronRight className='h-5 w-5' /></NavLink></li>
 
         <li><NavLink to="/contact" className={({ isActive }) => (isActive ? "navActive" : "navDefault")}>Contact Us <HiChevronRight className='h-5 w-5' /></NavLink></li>
+
+        <li className='lg:hidden'>
+            <Link to="/consultation" className="myBtn">Free Consultation</Link>
+        </li>
     </>
 
     const logoContainer = <>
@@ -26,9 +33,6 @@ const Navbar = () => {
         <div className="navbar bg-base-100">
             <div className="navbar-start">
                 <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </div>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                         {
                             navItem
@@ -46,8 +50,29 @@ const Navbar = () => {
                     }
                 </ul>
             </div>
-            <div className="navbar-end">
+            <div className="navbar-end hidden lg:flex">
                 <Link to="/consultation" className="myBtn">Free Consultation</Link>
+            </div>
+
+            <div className="navbar-end flex lg:hidden">
+                <label className="btn btn-circle swap swap-rotate">
+
+                    {/* this hidden checkbox controls the state */}
+                    <input type="checkbox" />
+
+                    {/* hamburger icon */}
+                    <svg onClick={() => setIsOpen(!isOpen)} className="swap-off fill-primary" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" /></svg>
+
+                    {/* close icon */}
+                    <svg onClick={() => setIsOpen(!isOpen)} className="swap-on fill-error" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" /></svg>
+
+                </label>
+
+
+                <ul className={`${isOpen ? "absolute top-16 left-0 space-y-5 flex flex-col items-center bg-white w-full py-6 transition-all duration-300" : "absolute -top-96 left-0 space-y-5 flex flex-col items-center bg-success bg-opacity-40 w-full py-6 transition-all duration-300"}`}>
+                    {navItem}
+                </ul>
+
             </div>
         </div>
     );
